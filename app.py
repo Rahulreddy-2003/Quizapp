0298755f987132a05fd274eb87df716e3d32e757
Rahul <rahulreddymandala2003@gmail.com>
2024-03-27 16:48:51 +0530

    removed app.run() for vercel

--- app.py
+++ app.py
@@ -161,6 +161,5 @@ def get_scores():
 @app.route("/dashboard.html")
 def dash_board():
     return render_template("dashboard.html")
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
+    
